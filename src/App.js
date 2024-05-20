54c3fd7a773b9d784e65d0ba4d1bc2b52503161d
wu-jeffrey <jeff.jiang.wu@gmail.com>
2024-05-20 16:59:42 -0400

    [init] add tailwind

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-white">tour-to</h1>
+      </div>
     </div>
   );
 }
